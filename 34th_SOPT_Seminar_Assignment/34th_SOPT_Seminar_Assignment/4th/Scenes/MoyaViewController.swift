7ebba428c8fde46406c3bf2ca2a2b355bb297dc3
FpRaArNkK <mspark1184@naver.com>
2024-05-10 03:59:28 +0900

    Feat: MoyaVC init

--- 34th_SOPT_Seminar_Assignment/34th_SOPT_Seminar_Assignment/4th/Scenes/MoyaViewController.swift
+++ 34th_SOPT_Seminar_Assignment/34th_SOPT_Seminar_Assignment/4th/Scenes/MoyaViewController.swift
@@ -5,4 +5,50 @@
 //  Created by 박민서 on 5/10/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class MoyaViewController: UIViewController {
+    
+    // MARK: Variables
+    
+    // MARK: Views
+    
+    // MARK: Life Cycle - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+        setUpStyle()
+        setUpLayout()
+        setUpConstraint()
+    }
+    
+    // MARK: setUpView
+    private func setUpView() {
+        self.view.backgroundColor = .black
+        
+    }
+    
+    // MARK: setUpStyle
+    private func setUpStyle() {
+
+    }
+    
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        [
+
+        ].forEach { self.view.addSubview($0) }
+    }
+    
+    // MARK: setUpConstraint
+    private func setUpConstraint() {
+
+    }
+}
+
+#Preview {
+    MoyaViewController()
+}
+
