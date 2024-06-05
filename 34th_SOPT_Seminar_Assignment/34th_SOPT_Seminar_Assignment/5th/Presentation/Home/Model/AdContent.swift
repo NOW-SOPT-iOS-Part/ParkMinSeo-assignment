42b86686e335df45644d387a19e50c13c6b02895
FpRaArNkK <mspark1184@naver.com>
2024-06-06 05:36:43 +0900

    Feat: Home 화면 필요 Model 작성

---
+++ 34th_SOPT_Seminar_Assignment/34th_SOPT_Seminar_Assignment/5th/Presentation/Home/Model/AdContent.swift
@@ -0,0 +1,16 @@
+//
+//  AdContent.swift
+//  34th_SOPT_Seminar_Assignment
+//
+//  Created by 박민서 on 6/6/24.
+//
+
+import Foundation
+
+/// 광고 콘텐츠 셀에 사용되는 모델입니다.
+struct AdContent {
+    /// 컨텐츠 ID
+    let contentId: Int
+    /// 이미지
+    let image: String
+}
---
+++ 34th_SOPT_Seminar_Assignment/34th_SOPT_Seminar_Assignment/5th/Presentation/Home/Model/MainCVSection.swift
@@ -0,0 +1,44 @@
+//
+//  MainCVSection.swift
+//  34th_SOPT_Seminar_Assignment
+//
+//  Created by 박민서 on 6/6/24.
+//
+
+import Foundation
+import UIKit
+
+enum MainCVSection: Hashable, CaseIterable {
+    /// 최상단 캐러셀 섹션
+    case topCarousel
+    /// 티빙 추천 콘텐츠 섹션
+    case recommend
+    /// 라이브 섹션
+    case stream
+    /// 1화 무료 이벤트 섹션
+    case event
+    /// 광고 섹션
+    case ads
+    /// 신비로운 영화 섹션
+    case fantastic
+}
+
+extension MainCVSection {
+    func getHeaderContent() -> HeaderContent {
+        switch self {
+            
+        case .topCarousel:
+            return .init(labelTitle: "", showButton: false)
+        case .recommend:
+            return .init(labelTitle: "티빙에서 꼭 봐야하는 콘텐츠", showButton: true)
+        case .stream:
+            return .init(labelTitle: "인기 LIVE 채널", showButton: true)
+        case .event:
+            return .init(labelTitle: "1화 무료! 파라마운트 + 인기 시리즈", showButton: true)
+        case .ads:
+            return .init(labelTitle: "", showButton: false)
+        case .fantastic:
+            return .init(labelTitle: "마술보다 더 신비로운 영화(신비로운 영화사전님)", showButton: true)
+        }
+    }
+}
