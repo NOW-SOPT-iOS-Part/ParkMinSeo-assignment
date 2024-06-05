f794d1d3b4f1b780da9a928c4804c2995b26501d
FpRaArNkK <mspark1184@naver.com>
2024-06-06 05:26:56 +0900

    Feat: HomeViewModel init

---
+++ 34th_SOPT_Seminar_Assignment/34th_SOPT_Seminar_Assignment/5th/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewModel.swift
+//  34th_SOPT_Seminar_Assignment
+//
+//  Created by 박민서 on 6/6/24.
+//
+
+import RxSwift
+
+final class HomeViewModel: ViewModelType {
+    
+    // MARK: Properties
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        <#code#>
+    }
+}
